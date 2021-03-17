1d0259868cce5b601e8cb01cf2fcbc6afd30c142
BettyHoPro <bettyvisualtech@gmail.com>
2021-03-16 22:14:11 -0700

    add the empty conponent

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { action } from "@storybook/addon-actions";
+// import { action } from "@storybook/addon-actions";
 
 export default function Header(props) {
   return (
     <header className="appointment__time">
-      <h4 className="text--semi-bold">{props.time}}</h4>
+      <h4 className="text--semi-bold">{props.time}</h4>
       <hr className="appointment__separator" />
     </header>
   );
