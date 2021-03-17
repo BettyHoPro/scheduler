fb07f120214ce6d91dfc00fcd548fe442516e194
BettyHoPro <bettyvisualtech@gmail.com>
2021-03-16 18:34:09 -0700

    InterviewerList and InterviewerListItem second base

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,5 +5,15 @@ import InterviewerListItem from "components/InterviewerListItem";
 
 
 export default function InterviewerList(props) {
-
+  const interviewer = props.interviewers.map((interviewer) => {
+    return (
+      <interviewerListItem 
+      key={interviewer.id}
+      name={interviewer.name} 
+      avatar={interviewer.avatar} 
+      selected={interviewer.name === props.interviewer}
+      setInterviewer={props.setInterviewer} />
+    )
+  });
 };
+
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,5 +3,24 @@ import classnames from "classnames";
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
+  let interviewerClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+    "interviewers__item-image": props.avatar,
+  });
+  // id:number - the id of the interviewer
+  // name:string - the name of the interviewer
+  // avatar:url - a url to an image of the interviewer
+  // selected:boolean - to determine if an interview is selected or not
+  // setInterviewer:function - sets the interviewer upon selection
 
-};
+  return (
+    <li className="interviewers__item" onClick={() => props.setInterviewer(props.name)}>
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      Sylvia Palmer
+    </li>
+  );
+}
