3ecc98bb39bfcc2b2aa26aaee7ca6d7fd8c06a6e
BettyHoPro <bettyvisualtech@gmail.com>
2021-03-17 08:16:25 -0700

    modify the confirm conponent

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Button from "components/Button";
 
 export default function Deleting(props) {
   return (
     <main className="appointment__card appointment__card--confirm">
       <h1 className="text--semi-bold">{props.message}</h1>
       <section className="appointment__actions">
-        <Button danger>{props.onCancel}</Button>
-        <Button danger>{props.onConfirm}</Button>
+        <Button danger>{props.onCancel}Cancel</Button>
+        <Button danger>{props.onConfirm}Confirm</Button>
       </section>
     </main>
   );
