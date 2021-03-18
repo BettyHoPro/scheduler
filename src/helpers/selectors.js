1951aa02bd100a42c0d2084179c062b753b87837
BettyHoPro <bettyvisualtech@gmail.com>
2021-03-18 14:33:15 -0700

    add helper function selector

---
+++ src/helpers/selectors.js
@@ -0,0 +1,14 @@
+export function getAppointmentsForDay(state, day) {
+  // const copyState = {...state};
+  //get the selected day appointments' ids
+  const { days, appointments } = state;
+  if (days.length === 0) return [];
+  const checkDayExsit = days.filter((dayX) => dayX.name === day);
+  if (checkDayExsit.length > 0) {
+    const selectedDayAppoints = checkDayExsit[0].appointments;
+    return selectedDayAppoints.length > 0
+      ? selectedDayAppoints.map((appointID) => appointments[appointID])
+      : selectedDayAppoints;
+  }
+  return [];
+}
