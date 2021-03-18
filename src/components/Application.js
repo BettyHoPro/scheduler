1181ab664a247768b483052a212a56a2490423de
BettyHoPro <bettyvisualtech@gmail.com>
2021-03-18 15:58:00 -0700

    refactpry the useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,58 +5,65 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
 
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id:3,
-//     time:"2pm",
-//     interview: {
-//       student: "Alice Alison",
-//       interviewer: {
-//         id: 2,
-//         name:"Tori Malcolm",
-//         avatar: "https://i.imgur.com/Nmx0Qxo.png" 
-//       }
-//     } 
-//   },
-//   {
-//     id: 4,
-//     time: "10am"
-//   },
-//   {
-//     id:5,
-//     time:"5pm",
-//     interview: {
-//       student: "Tommy Chen",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy", 
-//         avatar: "https://i.imgur.com/FK8V841.jpg"
-//       }
-//     } 
-//   }
-// ];
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id:3,
+    time:"2pm",
+    interview: {
+      student: "Alice Alison",
+      interviewer: {
+        id: 2,
+        name:"Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png" 
+      }
+    } 
+  },
+  {
+    id: 4,
+    time: "10am"
+  },
+  {
+    id:5,
+    time:"5pm",
+    interview: {
+      student: "Tommy Chen",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy", 
+        avatar: "https://i.imgur.com/FK8V841.jpg"
+      }
+    } 
+  }
+];
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday"); // Monday-> initial state, day= Monday, setDay = function 
-  const [days, setDays] = useState([]);
-  const [appointments, setAppointments] = useState({});
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = (days) => setState({...state, days});
+
   useEffect(() => {
     axios.get("/api/days")
     .then((response) => {
@@ -79,8 +86,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            setDays={setDays}
+            days={state.days}
+            day={state.day}
+            setDay={setDay}
           />
         </nav>
         <img
