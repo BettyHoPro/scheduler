4f221ba9fb3bc849d586422032b9058f975ff875
BettyHoPro <bettyvisualtech@gmail.com>
2021-03-23 23:03:38 -0700

    add debug and expect

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -10,6 +10,7 @@ import {
   getAllByTestId, 
   getByAltText, 
   getByPlaceholderText,
+  toBeInTheDocument
 
 } from "@testing-library/react";
 
@@ -32,7 +33,7 @@ it("changes the schedule when a new day is selected", async() => {
 it("loads data, books an interview and reduces the spots remaining for Monday by 1", async() => {
 // Render the Application.
 
-const { container } = render(<Application />);
+const { container, debug } = render(<Application />);
 // Wait until the text "Archie Cohen" is displayed.
 await waitForElement(() => getByText(container, "Archie Cohen"));
 const appointment = getAllByTestId(container, "appointment")[0];
@@ -47,7 +48,10 @@ fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
 // Click the "Save" button on that same appointment.
 fireEvent.click(getByText(appointment, "Save"));
+
 // Check that the element with the text "Saving" is displayed.
+expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
 // Wait until the element with the text "Lydia Miller-Jones" is displayed.
 // Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
 
