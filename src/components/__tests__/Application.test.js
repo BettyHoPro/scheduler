04b6c2f42a3df52f61c795ccbd4942b8e7ea8554
BettyHoPro <bettyvisualtech@gmail.com>
2021-03-23 23:19:20 -0700

    clean out console.log and finish debugging tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,62 +1,62 @@
 import React from "react";
 
-import { 
-  render, 
-  cleanup, 
-  waitForElement, 
-  fireEvent, 
-  getByText, 
-  prettyDOM, 
-  getAllByTestId, 
-  getByAltText, 
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+  getByText,
+  prettyDOM,
+  getAllByTestId,
+  getByAltText,
   getByPlaceholderText,
-  toBeInTheDocument
-
+  toBeInTheDocument,
+  // getAllByTestId,
+  queryByText,
 } from "@testing-library/react";
 
 import Application from "components/Application";
 
-
-
 afterEach(cleanup);
 
 describe("Application", () => {
-it("changes the schedule when a new day is selected", async() => {
-  const { getByText } = render(<Application />);
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
 
-  await waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    await waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
-});
 
-it("loads data, books an interview and reduces the spots remaining for Monday by 1", async() => {
-// Render the Application.
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    // Render the Application.
 
-const { container, debug } = render(<Application />);
-// Wait until the text "Archie Cohen" is displayed.
-await waitForElement(() => getByText(container, "Archie Cohen"));
-const appointment = getAllByTestId(container, "appointment")[0];
-// Click the "Add" button on the first empty appointment.
-fireEvent.click(getByAltText(appointment, "Add"));
-// Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
-fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-  target: { value: "Lydia Miller-Jones" }
-});
-// Click the first interviewer in the list.
-fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    const { container } = render(<Application />);
+    // Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointment = getAllByTestId(container, "appointment")[0];
+    // Click the "Add" button on the first empty appointment.
+    fireEvent.click(getByAltText(appointment, "Add"));
+    // Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+    // Click the first interviewer in the list.
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
-// Click the "Save" button on that same appointment.
-fireEvent.click(getByText(appointment, "Save"));
+    // Click the "Save" button on that same appointment.
+    fireEvent.click(getByText(appointment, "Save"));
 
-// Check that the element with the text "Saving" is displayed.
-expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    // Check that the element with the text "Saving" is displayed.
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
 
-// Wait until the element with the text "Lydia Miller-Jones" is displayed.
-// Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
+    // Wait until the element with the text "Lydia Miller-Jones" is displayed.
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
 
-
-  console.log(prettyDOM(appointment));
-});
-
+    // Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+  });
 });
